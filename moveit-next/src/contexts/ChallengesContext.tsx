0e824241a19a91f8a37fe0abef42a5fc43ad1731
Ecthon Borhis <ecthon@gmail.com>
2021-02-25 12:43:41 -0400

    Add challenges

---
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -0,0 +1,64 @@
+import { createContext, useState, ReactNode } from "react";
+import challegens from "../../challenges.json";
+
+interface Challenge {
+  type: "body" | "eye";
+  description: string;
+  amount: number;
+}
+
+interface ChallegensContextData {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  experienceToNextLevel: number;
+  activeChallenge: Challenge;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+  resetChallenge: () => void;
+}
+
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
+
+export const ChallengesContext = createContext({} as ChallegensContextData);
+
+export function ChallengesProvider({ children }: ChallengesProviderProps) {
+  const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+  const [activeChallenge, setActiveChallenge] = useState(null);
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
+  function levelUp() {
+    setLevel(level + 1);
+  }
+
+  function startNewChallenge() {
+    const randomChallengeIndex = Math.floor(Math.random() * challegens.length);
+    const challegen = challegens[randomChallengeIndex];
+    setActiveChallenge(challegen);
+  }
+
+  function resetChallenge() {
+    setActiveChallenge(null);
+  }
+
+  return (
+    <ChallengesContext.Provider
+      value={{
+        level,
+        currentExperience,
+        challengesCompleted,
+        experienceToNextLevel,
+        levelUp,
+        startNewChallenge,
+        activeChallenge,
+        resetChallenge,
+      }}
+    >
+      {children}
+    </ChallengesContext.Provider>
+  );
+}
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -3,8 +3,8 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-import styles from "../styles/pages/Home.module.css";
 import { ChallengeBox } from "../components/ChallengeBox";
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
