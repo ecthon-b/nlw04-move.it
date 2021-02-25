d5d2c61fdeb8191e32af07747d277dd12e42487f
Ecthon Borhis <ecthon@gmail.com>
2021-02-25 09:35:17 -0400

    Finish component ChallengeBox

--- moveit-next/src/components/ChallengeBox.tsx
+++ moveit-next/src/components/ChallengeBox.tsx
@@ -1,11 +1,36 @@
-import styles from '../styles/components/ChallengeBox.module.css';
+import styles from "../styles/components/ChallengeBox.module.css";
 
 export function ChallengeBox() {
-    return(
-        <div className="styles.challengeBoxContainer">
-            <div className={styles.challengeNotActive}>
-                <strong>Inicia um ciclo para receber desafios a serem completados</strong>
-            </div>
+  const hasActiveChallenge = true;
+  return (
+    <div className={styles.challengeBoxContainer}>
+      {hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400 xp</header>
+          <main>
+            <img src="icons/body.svg" alt="" />
+            <strong>Novo desafio</strong>
+            <p>Levante e faça uma panelada de brigadeiro.</p>
+          </main>
+
+          <footer>
+            <button type="button" className={styles.challengeFailedButton}>
+              Falhei
+            </button>
+            <button type="button" className={styles.challengeSucceededButton}>
+              Completei
+            </button>
+          </footer>
         </div>
-    )
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>Finalize um ciclo para receber um desafio</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up" />
+            Avanve de level completando os desafios.
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,10 +1,10 @@
+import Head from "next/head";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-import styles from '../styles/pages/Home.module.css'
-
-import Head from 'next/head'
+import styles from "../styles/pages/Home.module.css";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -20,8 +20,10 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
-  )
+  );
 }
